752ee353d644141a5a4013ded95917ee8c5c5a87
wangchi <wangchi92@gmail.com>
2017-01-31 23:31:13 +0800

    - Change the feedbackToUser variable to private - Added getFeedbackToUser() function to return feedbackToUser - Changed all other classes that ask for feedbackToUser to ask for getFeedbackToUser() instead

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,8 +11,13 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private final String feedbackToUser;
 
+    /** The getter function for the above variable */
+    public String getFeedbackToUser() {
+    	return feedbackToUser;
+    }
+    
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
 

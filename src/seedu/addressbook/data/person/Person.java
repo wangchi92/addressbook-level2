9f4e05b64fddd58f3e6e1580dc6c72b3bb053f7b
wangchi <wangchi92@gmail.com>
2017-02-07 01:01:57 +0800

    T4A2

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -10,10 +10,13 @@ import java.util.Objects;
  */
 public class Person implements ReadOnlyPerson {
 
+	private static int nextSequenceNumber=1;
+	
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
+    private int sequenceNumber;
 
     private final UniqueTagList tags;
     /**
@@ -24,6 +27,8 @@ public class Person implements ReadOnlyPerson {
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.sequenceNumber = nextSequenceNumber;
+        Person.nextSequenceNumber++;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
     }
 
@@ -54,6 +59,10 @@ public class Person implements ReadOnlyPerson {
         return address;
     }
 
+    public int getSequenceNumber() {
+    	return sequenceNumber;
+    }
+    
     @Override
     public UniqueTagList getTags() {
         return new UniqueTagList(tags);
